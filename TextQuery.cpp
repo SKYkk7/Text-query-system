8b58125801b8c1ef691d77612bbdd7b7a6e8f7c7
1348096084@qq.com <1348096084@qq.com>
2020-09-20 16:58:12 +0800

    第一次修改

--- TextQuery.cpp
+++ TextQuery.cpp
@@ -4,11 +4,17 @@
 
 TextQuery::TextQuery(std::ifstream& ifs) : input(new std::vector<std::string>)
 {
-    LineNo lineNo{ 0 };
-    for (std::string line; std::getline(ifs, line); ++lineNo) {
+    LineNo lineNo{ 0 };  // 初始化
+
+    // 对文本每一行进行查询，并用lineNo来记录所查单词所在行号
+    for (std::string line; std::getline(ifs, line); ++lineNo)
+    {
         input->push_back(line);
         std::istringstream line_stream(line);
-        for (std::string text, word; line_stream >> text; word.clear()) {
+
+        // 对每一行中每一个单词进行匹配，
+        for (std::string text, word; line_stream >> text; word.clear())
+        {
         
             std::remove_copy_if(text.begin(), text.end(), std::back_inserter(word), ispunct);
            
@@ -19,16 +25,20 @@ TextQuery::TextQuery(std::ifstream& ifs) : input(new std::vector<std::string>)
     }
 }
 
+// 查询函数
 QueryResult TextQuery::query(const std::string& str) const
 {
     
     static std::shared_ptr<std::set<LineNo>> nodata(new std::set<LineNo>);
     auto found = result.find(str);
-    if (found == result.end()) return QueryResult(str, nodata, input);
-    else return QueryResult(str, found->second, input);
+    if (found == result.end())  
+        return QueryResult(str, nodata, input);  // 查找成功
+    else 
+        return QueryResult(str, found->second, input);   // 查找失败
 }
 
-std::ostream& print(std::ostream& out, const QueryResult& qr)
+// 打印函数
+std::ostream& print(std::ostream& out, const QueryResult& qr)  
 {
     out << qr.word << " occurs " << qr.nos->size() << (qr.nos->size() > 1 ? " times" : " time") << std::endl;
     for (auto i : *qr.nos)
--- TextQuery.h
+++ TextQuery.h
@@ -11,28 +11,31 @@ class QueryResult;
 #include <set>
 
 class QueryResult;
+
+// 保存输入文本
 class TextQuery {
 public:
     using LineNo = std::vector<std::string>::size_type;
-    TextQuery(std::ifstream&);
-    QueryResult query(const std::string&) const;
+    TextQuery(std::ifstream&);   // 通过一个输入流来构造
+    QueryResult query(const std::string&) const;  // 接受一个字符串进行查询，返回一个QueryResult
 private:
-    std::shared_ptr<std::vector<std::string>> input;
-    std::map<std::string, std::shared_ptr<std::set<LineNo>>> result;
+    std::shared_ptr<std::vector<std::string>> input;   // 保存输入的文本
+    std::map<std::string, std::shared_ptr<std::set<LineNo>>> result;   // 查询结果
 };
 
+// 保存查询结果的类
 class QueryResult {
 public:
-    friend std::ostream& print(std::ostream&, const QueryResult&);
+    friend std::ostream& print(std::ostream&, const QueryResult&);    // 通过一个输出流来打印查询结果
 public:
     QueryResult(const std::string& s, std::shared_ptr<std::set<TextQuery::LineNo>> set, std::shared_ptr<std::vector<std::string>> v) : word(s), nos(set), input(v) { }
 private:
-    std::string word;
-    std::shared_ptr<std::set<TextQuery::LineNo>> nos;
-    std::shared_ptr<std::vector<std::string>> input;
+    std::string word;  // 查询的单词
+    std::shared_ptr<std::set<TextQuery::LineNo>> nos;   // 通过set保存单词所在行号
+    std::shared_ptr<std::vector<std::string>> input;    // 保存输入的文本
 };
 
-std::ostream& print(std::ostream&, const QueryResult&);
+std::ostream& print(std::ostream&, const QueryResult&);   
 
 #endif // !TEXTQUERY_H
 
